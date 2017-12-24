f57c3722a03ff57b168492107e821f0582c1dbb0
100297 <league@WTS7.attlocal.net>
2017-12-24 11:29:47 -0800

    square
    asdfasdf

--- src/IntroToJUnitTest/MoreAndMoreTests.java
+++ src/IntroToJUnitTest/MoreAndMoreTests.java
@@ -6,71 +6,86 @@ import org.junit.Test;
 
 /**
  * Uncomment each test and then write the method to make it pass.
- * **/
+ **/
 
 public class MoreAndMoreTests {
 
 	@Test
-  public void MultTest() {
-	
+	public void MultTest() {
+
 		assertEquals("10 x 0 = 0", multiply(10, 0));
-	    assertEquals("10 x 10 = 100", multiply(10, 10));
-	    assertEquals("8 x 11 = 88", multiply(8, 11));
-	    
- }
-//
+		assertEquals("10 x 10 = 100", multiply(10, 10));
+		assertEquals("8 x 11 = 88", multiply(8, 11));
+
+	}
+
+	//
 	@Test
-  public void PrimeTest() {
+	public void PrimeTest() {
 
-    assertTrue(isPrime(3));
-	    assertTrue(isPrime(5));
-	    assertTrue(isPrime(541));
-	    assertFalse(isPrime(4));
-	    assertFalse(isPrime(12));
-	    assertFalse(isPrime(527));
+		assertTrue(isPrime(3));
+		assertTrue(isPrime(5));
+		assertTrue(isPrime(541));
+		assertFalse(isPrime(4));
+		assertFalse(isPrime(12));
+		assertFalse(isPrime(527));
 
-	  }
-////
-///	@Test
-//	  public void SquareTest() {
-//
-//	    assertTrue(isSquare(4));
-//	    assertTrue(isSquare(144));
-//	    assertTrue(isSquare(64));
-//	    assertTrue(isSquare(10201));
-//	    assertTrue(isSquare(1));
-//	    assertFalse(isSquare(3));
-//	    assertFalse(isSquare(22));
-//	    assertFalse(isSquare(143));
-//
-//	  }
-//
-//	@Test
-//	  public void CubeTest() {
-//
-//	    assertTrue(isCube(27));
-//	    assertTrue(isCube(216));
-//	    assertTrue(isCube(729));
-//	    assertTrue(isCube(1));
-//	    assertFalse(isCube(3));
-//	    assertFalse(isCube(22));
-//	    assertFalse(isCube(143));
-//
-//	  }
-
-private String multiply(int i, int j) {
-	// TODO Auto-generated method stub
-	String name = "" + i + " x " + j + " = " + i*j;
-	System.out.println("" + name);
-	//10 x 0 = 0
-	return name;
-}
-private boolean isPrime(int x) {
+	}
+///*//	//
+	@Test
+	 public void SquareTest() {
 	
-	return true;
+ assertTrue(isSquare(4));
+	 assertTrue(isSquare(144));
+	 assertTrue(isSquare(64));
+	 assertTrue(isSquare(10201));
+	 assertTrue(isSquare(1));
+	 assertFalse(isSquare(3));
+	 assertFalse(isSquare(22));
+	 assertFalse(isSquare(143));
 	
-}
+	 }	
+	// @Test
+	// public void CubeTest() {
+	//
+	// assertTrue(isCube(27));
+	// assertTrue(isCube(216));
+	// assertTrue(isCube(729));
+	// assertTrue(isCube(1));
+	// assertFalse(isCube(3));
+	// assertFalse(isCube(22));
+	// assertFalse(isCube(143));
+	//
+	// }
 
+	private boolean isSquare(int i) {
+		// TODO Auto-generated method stub
+		int dad = i;
+		for (int j = 1; j < dad; j++) {
+			if (j*j==dad) {
+		return true;
+		}
+		}
+		return false;
+	}
 
+	private String multiply(int i, int j) {
+		// TODO Auto-generated method stub
+		String name = "" + i + " x " + j + " = " + i * j;
+		System.out.println("" + name);
+		// 10 x 0 = 0
+		return name;
+	}
+
+	private boolean isPrime(int x) {
+		int mom = x;
+		for (int j = 2; j < mom; j++) {
+			if (mom % j == 0) {
+				return false;
+			}
+		}
+		return true;
+
+	}
 
 }
