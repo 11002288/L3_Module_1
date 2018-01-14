2c8f106d82901124dcf6cd64a137941743b0c12a
11002288 <league@WTS7.attlocal.net>
2018-01-14 11:28:16 -0800

    You do not need message 2 method
    Test does not show up

--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
-		 //<- this needs changing
+		//<- this needs changing
 		int c = 0;
 		for (int i = 0; i < eggs.size(); i++) {
 			if (eggs.get(i).equals("cracked")) {
@@ -25,16 +25,41 @@ public class Algorithms {
 		return j;
 	}
 
-	public static int findTallest(List<Double> peeps) {
+	public static Double findTallest(List<Double> peeps) {
 		// TODO Auto-generated method stub
-		
-		//for (int i = 0; i < peeps.size(); i++) {
-		//	if (peeps.get(i)==) {
-				
-//	}	
-	//	}
-		return (int) 6.6;
+		Double w = 0.0;
+		for (int i = 0; i < peeps.size(); i++) {
+			if (peeps.get(i)>=w) {
+				w = peeps.get(i);
+
+			}	
+
+		}
+		return w;
+
 	}
-	
+
+	public static String findLongestWord(List<String> words) {
+		// TODO Auto-generated method stub
+		String m = "";
+		for (int i = 0; i < words.size(); i++) {
+			if (words.get(i).length()>=m.length()) {
+				m = words.get(i);
+			}
+		}
+		return m;
+	}
+
+	public static Object containsSOS(List<String> message1) {
+		// TODO Auto-generated method stub
+		String o = "... --- ...";
+		for (int i = 0; i < message1.size(); i++) {
+			if (message1.get(i).contains(o)) {
+				System.out.println("test");
+			}
+		}
+		return o;
+	}
+
 	//Add other methods here
 }
